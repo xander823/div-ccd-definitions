e0cd6c978dc8329377f22ab7b676d6ebdd644eca
xander823 <42318446+xander823@users.noreply.github.com>
2021-03-07 09:26:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const healthcheck = require('@hmcts/nodejs-healthcheck');
 
 const app = express();
-const payload = { message: 'Im all right !' };
+const payload = { message: 'Im alright !' };
 const config = require('@hmcts/properties-volume').addTo(require('config'));
 
 const port = config.get('server.port');
